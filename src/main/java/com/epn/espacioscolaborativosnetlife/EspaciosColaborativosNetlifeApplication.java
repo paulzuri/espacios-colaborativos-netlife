4bd86719aceb870aa021639a53a53577e5be33d6
Paul <paulzuri@hotmail.com>
2023-10-31 21:43:46 -0500

    first commit

---
+++ src/main/java/com/epn/espacioscolaborativosnetlife/EspaciosColaborativosNetlifeApplication.java
@@ -0,0 +1,13 @@
+package com.epn.espacioscolaborativosnetlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspaciosColaborativosNetlifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspaciosColaborativosNetlifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epn/espacioscolaborativosnetlife/EspaciosColaborativosNetlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epn.espacioscolaborativosnetlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspaciosColaborativosNetlifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
